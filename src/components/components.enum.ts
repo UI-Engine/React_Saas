c6d38f519492bf86bb3740dfa60aa35dd9e5b4cd
karbasu <karbasappabembulge@gmail.com>
2025-04-23 02:11:04 +0530

    listed basic components

---
+++ src/components/components.enum.ts
@@ -0,0 +1,67 @@
+export enum CoreComponents {
+    Container = "Container",
+    Box = "Box",
+    Grid = "Grid",
+    Flex = "Flex",
+    Stack = "Stack",
+    Typography = "Typography",
+    Image = "Image",
+    Divider = "Divider",
+    Avatar = "Avatar",
+    Badge = "Badge",
+    Card = "Card",
+    Button = "Button",
+    Input = "Input",
+    Textarea = "Textarea",
+    Checkbox = "Checkbox",
+    Radio = "Radio",
+    Switch = "Switch",
+    Select = "Select",
+    Slider = "Slider",
+    DatePicker = "DatePicker",
+    TimePicker = "TimePicker",
+    Toast = "Toast",
+    Modal = "Modal",
+    Alert = "Alert",
+    Tooltip = "Tooltip",
+    Popover = "Popover",
+    Skeleton = "Skeleton",
+    Navbar = "Navbar",
+    Tabs = "Tabs",
+    Breadcrumbs = "Breadcrumbs",
+    Pagination = "Pagination",
+    Drawer = "Drawer",
+    Accordion = "Accordion"
+  }
+  
+  export enum UniqueComponents {
+    CommandPalette = "CommandPalette",
+    DataTable = "DataTable",
+    Stepper = "Stepper",
+    KanbanBoard = "KanbanBoard",
+    FileDropzone = "FileDropzone",
+    RichTextEditor = "RichTextEditor",
+    ChatBubble = "ChatBubble",
+    DraggablePanel = "DraggablePanel",
+    JSONViewer = "JSONViewer",
+    Timeline = "Timeline"
+  }
+  
+  export enum ComplexComponents {
+    SwipeAbleCards = "SwipeAbleCards",
+    LiveCursorOverlay = "LiveCursorOverlay",
+    NotificationCenter = "NotificationCenter",
+    VirtualList = "VirtualList",
+    DynamicFormBuilder = "DynamicFormBuilder",
+    ChartWrapper = "ChartWrapper"
+  }
+  
+  export enum HardComponents {
+    AccessibleDropdown = "AccessibleDropdown",
+    CustomDragDrop = "CustomDragDrop",
+    ThemingSystem = "ThemingSystem",
+    AnimatedTransitions = "AnimatedTransitions",
+    ScrollTriggeredAnimation = "ScrollTriggeredAnimation",
+    EditableDataGrid = "EditableDataGrid"
+  }
+  
